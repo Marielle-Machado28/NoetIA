385d5adcdeb706d99c81bd81f87487cfbbb899cb
Marielle Lozano Machado <marielle280.machado@gmail.com>
2026-02-21 03:55:25 -0600

    sync con google

--- app/pages/Calendario.py
+++ app/pages/Calendario.py
@@ -1,5 +1,7 @@
 import sys
 import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import streamlit as st
 from streamlit_calendar import calendar
 from pathlib import Path
