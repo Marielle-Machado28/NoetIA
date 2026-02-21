bfea3d682bfe91af9cddb7cbc9f9912b8af7da24
Marielle Lozano Machado <marielle280.machado@gmail.com>
2026-02-21 03:49:56 -0600

    sync con google

--- app/pages/Calendario.py
+++ app/pages/Calendario.py
@@ -2,12 +2,15 @@ import sys
 import os
 import streamlit as st
 from streamlit_calendar import calendar
+from pathlib import Path
 
 # 1. Configuración de página (Única y primera)
 st.set_page_config(page_title="NoetIA", layout="wide")
 
 # 2. Configuración de paths (para que el sidebar encuentre tus módulos)
-root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+app_path = Path(__file__).resolve().parent
+# 2. Configuración de rutas
+root_path = app_path.parent
 src_path = os.path.join(root_path, 'src')
 if src_path not in sys.path:
     sys.path.insert(0, src_path)
